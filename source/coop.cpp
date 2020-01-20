f71b5f5c37b89d1553ce6a1a359edf100253199a
tkgstrator <tkgstrator@users.noreply.github.com>
2020-01-20 16:12:19 -0500

    Optimization.

--- source/coop.cpp
+++ source/coop.cpp
@@ -1,6 +1,5 @@
 #include "coop.h"
 #include "sead/random.h"
-#include <utility>
 
 namespace coop
 {
@@ -10,31 +9,28 @@ void Seedhack::init(u32 firstseed)
     seed = firstseed;
 }
 
-u32 Seedhack::getWaveInfo()
+std::string Seedhack::getWaveInfo()
 {
     sead::Random rnd;
     rnd.init(seed);
 
     struct Wave mWave;
     struct Prob mProb;
-    u64 sum = 0;
-    u32 info = 0;
-
-    for (u32 wave = 0; wave < 3; wave++)
+    std::string waveinfo = "";
+    
+    for (u16 wave = 0; wave < 3; ++wave)
     {
-        for (u32 event = 0; event < 7; event++)
+        for (u64 event = 0, sum = 0; event < 7; ++event)
         {
             if ((wave > 0) && (mWave.event[wave - 1] != 0) && (mWave.event[wave - 1] == event))
                 continue;
             sum += mProb.event[event];
-            if ((rnd.getU32() * sum >> 0x20) < mProb.event[event])
+            if ((rnd.getU32()  *sum >> 0x20) < mProb.event[event])
             {
                 mWave.event[wave] = event;
             }
         }
-        // if (mWevent[wave] != )
-        sum = 0; // Re initialize
-        for (u32 tide = 0; tide < 3; tide++)
+        for (u64 tide = 0, sum = 0; tide < 3; ++tide)
         {
             if ((tide == 0) && (1 <= mWave.event[wave] && (mWave.event[wave] <= 3)))
                 continue;
@@ -42,34 +38,33 @@ u32 Seedhack::getWaveInfo()
             if ((rnd.getU32() * sum >> 0x20) < mProb.tide[tide])
                 mWave.event[wave] == 6 ? mWave.tide[wave] = 0 : mWave.tide[wave] = tide;
         }
-        info = info * 100 + mWave.tide[wave] * 10 + mWave.event[wave];
+        waveinfo += std::to_string(mWave.tide[wave]) + std::to_string(mWave.event[wave]);
     }
-    return info;
+    return waveinfo;
 }
 
-void Seedhack::getGeyser(u32 size)
+std::vector<std::string> Seedhack::getGeyser(u32 size)
 {
     sead::Random rnd;
+    std::vector<std::string> gpos(3);
     rnd.init(seed);
-
     rnd.getU32();
     
     u32 gameSeed[3] = {seed, rnd.getU32(), rnd.getU32()};
 
-    for (u32 wave = 0; wave < 3; wave++)
+    for (u16 wave = 0; wave < 3; ++wave)
     {
         sead::Random grnd;
         grnd.init(gameSeed[wave]);
         u64 gArray[size];
         // Initialize Array of Geyser
-        gpos[wave] = "";
-        for (u64 j = 0; j < size; j++)
+        for (u64 j = 0; j < size; ++j)
         {
             gArray[j] = j;
         }
-        for (u64 i = 0; i < 12; i++)
+        for (u16 i = 0; i < 10; ++i)
         {
-            for (u64 sel = size - 1; sel > 0; sel--)
+            for (u64 sel = size - 1; sel > 0; --sel)
             {
                 u64 index = (grnd.getU32() * (sel + 1)) >> 0x20;
                 std::swap(gArray[sel], gArray[index]);
@@ -77,5 +72,6 @@ void Seedhack::getGeyser(u32 size)
             gpos[wave] += std::to_string(gArray[0]);
         }
     }
+    return gpos;
 }
 }; // namespace coop
--- source/main.cpp
+++ source/main.cpp
@@ -7,54 +7,40 @@
 #include <string>
 #include <time.h>
 #include <omp.h>
-#define N 0xFFFFFF
-#define Q "11111111"
+#define Q "1111111"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    ostringstream sout;
-    coop::Seedhack tkg;
+    if (argc < 2)
+        return -1;
+    string wave = argv[1]; 
+    u32 range = atof(argv[2]); 
+    // if (argc == 3)
+    //     std::string geyser = argv[3];
+
+    cout << "Hello Starlight!" << endl;
     ofstream ofs("seeds.txt");
     clock_t start = clock();
 
-    cout << "Hello Starlight!" << endl;
 #ifdef _OPENMP
-    cout << "Threads:" << omp_get_max_threads() << ", Num:" << N << endl;
+    u16 threads = omp_get_max_threads();
+    cout << "Threads:" << omp_get_max_threads() << ", Num:" << range << endl;
+#else
+    u16 threads = 1;
 #endif
-#pragma omp parallel for num_threads(1)
-    for (u32 seed = 0x0; seed < N; seed++)
+#pragma omp parallel for
+    for (u32 seed = 0x0; seed < range; ++seed)
     {
+        coop::Seedhack tkg;
         tkg.init(seed);
-        sout.str("");
-#pragma omp critical
-        sout << setfill('0') << right << setw(6) << dec << tkg.getWaveInfo();
-        string info = sout.str();
+        string info = tkg.getWaveInfo();
 
-        string::size_type index[2] = {info.find("22"), info.rfind("22")};
-        if ((index[0] != string::npos) && (index[0] % 2 == 0))
-        {
-            tkg.getGeyser(4);                                  // Calculate
-            if ((index[0] != index[1]) && (index[1] % 2 == 0)) // W1 and W3
-            {
-                if (tkg.gpos[0].find(Q) != string::npos || tkg.gpos[2].find(Q) != string::npos)
-                {
-                    ofs << "0x" << setw(8) << uppercase << setfill('0') << hex << seed;
-                    ofs << "," << info << "," << tkg.gpos[0] << "," << tkg.gpos[2] << endl;
-                }
-            }
-            else
-            {
-                if (tkg.gpos[info.find("22") / 2].find(Q) != string::npos)
-                {
-                    ofs << "0x" << setw(8) << uppercase << setfill('0') << hex << seed;
-                    ofs << "," << info << "," << tkg.gpos[info.find("22") / 2] << endl;
-                }
-            }
-        }
+        // std::vector<std::string> gpos = tkg.getGeyser(4);
     }
     clock_t end = clock();
-    const double time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
-    cout << time << "s" << endl;
+    const double time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0 / threads;
+    cout << time << "ms" << endl;
+    return 0;
 }

ad059ab4c74a47810762695284905e91c1d5c6d5
tkgstrator <nasawake.am@gmail.com>
2020-05-24 12:13:19 -0400

    Update calculate geyser position.

--- source/coop.cpp
+++ source/coop.cpp
@@ -1,33 +1,13 @@
 #include "coop.h"
-#include "sead/random.h"
+
 #include <iostream>
 
 namespace Coop
 {
-    class Setting {
-
-    };
-
-    class Geyser {
-
-    };
-
-    
-
-
-    void Seedhack::init(u32 firstseed)
+    void Setting::getWaveInfo(std::string mWaveInfo)
     {
-        seed = firstseed;
-    }
-
-    std::string Seedhack::getWaveInfo()
-    {
-        sead::Random rnd;
-        rnd.init(seed);
         struct Wave mWave;
         struct Prob mProb;
-        std::string waveinfo = "";
-
         for (u16 wave = 0; wave < 3; ++wave)
         {
             for (u64 event = 0, sum = 0; event < 7; ++event)
@@ -48,79 +28,82 @@ namespace Coop
                 if ((rnd.getU32() * sum >> 0x20) < mProb.tide[tide])
                     mWave.event[wave] == 6 ? mWave.tide[wave] = 0 : mWave.tide[wave] = tide;
             }
-            waveinfo += std::to_string(mWave.tide[wave]) + std::to_string(mWave.event[wave]);
+            std::string tmp = std::to_string(mWave.tide[wave]) + std::to_string(mWave.event[wave]);
+            if (mFast)
+            {
+                std::cout << "Seed: " << mSeed << " Comp: " << tmp << mWaveInfo.substr(wave * 2, 2) << std::endl;
+                if (mRegex)
+                    if (!std::regex_match(tmp, std::regex(mWaveInfo.substr(wave * 2, 2))))
+                        throw - 1;
+                if (mWaveInfo.substr(wave * 2, 2) != tmp)
+                    throw - 1;
+            }
+            mSetting += tmp;
         }
-        return waveinfo;
     }
 
-    std::vector<std::string> Seedhack::getGeyser(u16 stage_id)
+    Geyser::Geyser(u16 stage, u32 seed)
     {
-        sead::Random rnd;
-        std::vector<std::string> position(3);
         rnd.init(seed);
         rnd.getU32();
+        mGameSeed = {seed, rnd.getU32(), rnd.getU32()};
 
-        u32 gameSeed[3] = {seed, rnd.getU32(), rnd.getU32()};
-        std::vector<bool> reuse;
-        std::vector<std::string> geyser;
-        std::vector<std::vector<std::string>> goal;
+        switch (stage)
+        {
+        case 0:
+            mReuse = {true, true, true, true, true};
+            mSucc = {"E", "F", "G", "H", "I"};
+            mGoal = {{"H", "H"}, {"G", "H"}, {"F", "F"}, {"I", "F"}, {"H", "G"}};
+            break;
+        case 1:
+            // size = 4;
+            // rflag = false;
+            break;
+        case 2:
+            mReuse = {false, true, true, true, true};
+            mSucc = {"E", "F", "G", "H", "I"};
+            mGoal = {{"H", "H"}, {"G", "H"}, {"I", "F"}, {"I", "F"}, {"G", "H"}};
+            break;
+        case 3:
+            mReuse = {true, true, true, true, true};
+            mSucc = {"C", "D", "E", "F", "G"};
+            mGoal = {{"G", "E"}, {"F", "G"}, {"C", "C"}, {"D", "D"}, {"D", "C"}};
+            break;
+        case 4:
+            mReuse = {false, false, false, false};
+            mSucc = {"D", "E", "F", "G"};
+            mGoal = {{"G"}, {"F"}, {"G"}, {"F"}};
+            break;
+        default:
+            break;
+        }
+    }
 
+    void Geyser::getGeyser()
+    {
         for (u16 wave = 0; wave < 3; ++wave)
         {
-            sead::Random grnd;
-            grnd.init(gameSeed[wave]);
-            switch (stage_id)
-            {
-            case 0:
-                reuse = {true, true, true, true, true};
-                geyser = {"E", "F", "G", "H", "I"};
-                goal = {{"H", "H"}, {"G", "H"}, {"F", "F"}, {"I", "F"}, {"H", "G"}};
-                break;
-            case 1:
-                // size = 4;
-                // rflag = false;
-                break;
-            case 2:
-                reuse = {false, true, true, true, true};
-                geyser = {"E", "F", "G", "H", "I"};
-                goal = {{"H", "H"}, {"G", "H"}, {"I", "F"}, {"I", "F"}, {"G", "H"}};
-                break;
-            case 3:
-                reuse = {true, true, true, true, true};
-                geyser = {"C", "D", "E", "F", "G"};
-                goal = {{"G", "E"}, {"F", "G"}, {"C", "C"}, {"D", "D"}, {"D", "C"}};
-                break;
-            case 4:
-                reuse = {false, false, false, false};
-                geyser = {"D", "E", "F", "G"};
-                goal = {{"G"}, {"F"}, {"G"}, {"F"}};
-                break;
-            default:
-                break;
-            }
+            std::vector<bool> tReuse;
+            std::vector<std::string> tSucc;
+            std::vector<std::vector<std::string>> tGoal;
+            std::copy(mSucc.begin(), mSucc.end(), std::back_inserter(tSucc));
+            std::copy(mReuse.begin(), mReuse.end(), std::back_inserter(tReuse));
+            std::copy(mGoal.begin(), mGoal.end(), std::back_inserter(tGoal));
+            
+            grnd.init(mGameSeed[wave]);
             for (u16 i = 0; i < 15; ++i)
             {
-                for (u64 sel = geyser.size() - 1; sel > 0; --sel)
+                for (u64 sel = tSucc.size() - 1; sel > 0; --sel)
                 {
                     u64 index = (grnd.getU32() * (sel + 1)) >> 0x20;
-                    std::swap(geyser[sel], geyser[index]);
-                    std::swap(reuse[sel], reuse[index]);
-                    std::swap(goal[sel], goal[index]);
+                    std::swap(tSucc[sel], tSucc[index]);
+                    std::swap(tReuse[sel], tReuse[index]);
+                    std::swap(tGoal[sel], tGoal[index]);
                 }
-                // position[wave] += geyser[0];
-
-                if (reuse[0])
-                {
-                    u64 index = (grnd.getU32() * (goal[0].size())) >> 0x20;
-                    // std::string goal = mStage.goal[sel][index];
-                    position[wave] += "(" + geyser[0] + goal[0][index] + ")";
-                }
-                else
-                {
-                    position[wave] += "(" + geyser[0] + goal[0][0] + ")";
-                }
+                mPosition[wave] += tSucc[0];
+                // mPosition[wave] += "(" + mSucc[0] + mGoal[0][!mReuse[0] ? 0 : (grnd.getU32() * (mGoal[0].size())) >> 0x20] + ")";
+                // mPosition[wave] += "(" + mSucc[0] + mGoal[0][!mReuse[0] ? 0 : (grnd.getU32() * (mGoal[0].size())) >> 0x20] + ")";
             }
         }
-        return position;
     }
-}; // namespace coop
+}; // namespace Coop

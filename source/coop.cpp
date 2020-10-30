cc21a4ff0b2ca5e159efcfed16ed815a55b79ca0
tkgstrator <tkgstrator@users.noreply.github.com>
2020-10-30 05:48:44 -0400

    Optimize getEnemyAppearId

--- source/coop.cpp
+++ source/coop.cpp
@@ -1,106 +1,297 @@
 #include "coop.h"
 #include <iostream>
 
+#define N 9
+#define LODWORD(x) ((uint32_t)(x & 0xFFFFFFFF))
+
 namespace Coop
 {
-    bool Setting::getWaveInfo(std::string mWaveInfo)
+    u8 WaveMgr::getEnemyAppearId(u64 random, u8 id)
     {
-        struct Wave mWave;
-        struct Prob mProb;
-        for (u16 wave = 0; wave < 3; ++wave)
+        u8 mArray[3] = {1, 2, 3};
+        u8 w9;
+        u8 x9, x10, x11, x12;
+        u8 *x7, *w7;
+        u64 w8, x8;
+        u8 v17;
+        u8 v5 = id; // mAppearId
+        u8 x6 = 3;  // mAppearIdMax
+
+        // // ここでw6が減る
+        // if (!(v5 & 0x80000000))
+        // {
+        //     if (!w6)
+        //         return v5; // 現在の値をそのまま返す（でもそんなことはないはず）
+        //     w7 = mArray;
+        //     w8 = w6 - 1;
+        //     do
+        //     {
+        //         v17 = w8;
+        //         w9 = *w7;
+        //         if (*w7 < v5)
+        //             break;
+        //         w6 -= w9 == v5;
+        //         if (w9 == v5)
+        //             break;
+        //         w8 = v17 - 1;
+        //         ++w7;
+        //     } while (v17);
+        // }
+        // if (w6 < 1)
+        //     return v5;
+
+        // if (!x6)
+        //     return v5;
+        u8 w6 = id == 3 ? 3 : id + 1; // mAppearIdMax
+        x7 = mArray;
+        x8 = random * w6 >> 0x20;
+
+        while (1)
         {
-            for (u64 event = 0, sum = 0; event < 7; ++event) // Calculate Wave Event
+            x9 = *x7;
+            x10 = x8 == 0 ? 0 : x8 - 1;
+            x11 = x8 == 0 ? *x7 : id;
+            x12 = x9 == v5 ? 5 : x8 == 0;
+            // if (x8)
+            //     x11 = id;
+            // else
+            //     x11 = *x7;
+            // if (x9 == v5)
+            //     x12 = 5;
+            // else
+            //     x12 = x8 == 0;
+            if (x9 != v5)
             {
-                if ((wave > 0) && (mWave.event[wave - 1] != 0) && (mWave.event[wave - 1] == event))
-                    continue;
-                sum += mProb.event[event];
-                if ((rnd.getU32() * sum >> 0x20) < mProb.event[event])
+                x8 = LODWORD(x10);
+                id = x11;
+            }
+            // if (x9 != v5)
+            //     x8 = LODWORD(x10);
+            // if (x9 != v5)
+            //     id = x11;
+            if ((x12 & 7) != 5 && x12 & 7)
+                break;
+            --x6;
+            ++x7;
+            if (!x6)
+                return v5;
+        }
+        // if (!x12)
+        //     return v5;
+        return id;
+    }
+
+    void WaveMgr::getWaveArray()
+    {
+        sead::Random mRnd; // オオモノ出現のための乱数
+
+        u8 mEnemyAppearId = 1;
+        u8 mPhase[35];
+
+        const u8 wave0[35] = {2, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 3, 3, 3, 3}; // 20
+        const u8 wave1[35] = {2, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 3, 3}; // 22
+        const u8 wave2[35] = {2, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0}; // 24
+
+        if (mWave == 0)
+            memcpy(mPhase, wave0, sizeof(u8) * 35);
+        if (mWave == 1)
+            memcpy(mPhase, wave1, sizeof(u8) * 35);
+        if (mWave == 2)
+            memcpy(mPhase, wave2, sizeof(u8) * 35);
+
+        for (u8 value : mPhase)
+        {
+            if (value == 2)
+                rnd.getU32(); // 無意味に乱数を消費
+
+            if (value == 0)
+            {
+                const u64 random = rnd.getU32();
+                mEnemyAppearId = Coop::WaveMgr::getEnemyAppearId(random, mEnemyAppearId);
+                printf("RN-> %08X %d\n", random, mEnemyAppearId);
+            }
+
+            // オオモノシャケ出現
+            if (value == 1)
+            {
+                mRnd.init(rnd.getU32());
+                u16 mRareId = 0;
+                for (u64 mProb = 0; mProb < 7; ++mProb)
                 {
-                    mWave.event[wave] = event;
+                    if (!(mRnd.getU32() * (mProb + 1) >> 0x20))
+                        mRareId = mProb;
                 }
-            }
-            for (u64 tide = 0, sum = 0; tide < 3; ++tide) // Calculate Wave Tide
-            {
-                if ((tide == 0) && (1 <= mWave.event[wave] && (mWave.event[wave] <= 3)))
-                    continue;
-                sum += mProb.tide[tide];
-                if ((rnd.getU32() * sum >> 0x20) < mProb.tide[tide])
-                    mWave.event[wave] == 6 ? mWave.tide[wave] = 0 : mWave.tide[wave] = tide;
-            }
-            mSetting += std::to_string(mWave.tide[wave]) + std::to_string(mWave.event[wave]);
-            if (mFast && mRegex) // mRegex is Enable
-                if (!std::regex_match(mSetting, std::regex(mWaveInfo.substr(0, (wave + 1) * 2))))
-                    return false;
-            if (mFast && !mRegex) // mRegex is Disable
-                if (mWaveInfo.substr(wave * 2, 2) != mSetting.substr(wave * 2, 2))
-                    return false;
-        }
-        return true;
-    }
-
-    Geyser::Geyser(u16 stage, u32 seed)
-    {
-        rnd.init(seed);
-        rnd.getU32();
-        mGameSeed = {seed, rnd.getU32(), rnd.getU32()}; // GameSeed for Calulating Geyser Position.
-
-        switch (stage)
-        {
-        case 0:
-            mReuse = {true, true, true, true, true};
-            mSucc = {"E", "F", "G", "H", "I"};
-            mGoal = {{"H", "H"}, {"G", "H"}, {"F", "F"}, {"I", "F"}, {"H", "G"}};
-            break;
-        case 1:
-            // size = 4;
-            // rflag = false;
-            break;
-        case 2:
-            mReuse = {false, true, true, true, true};
-            mSucc = {"E", "F", "G", "H", "I"};
-            mGoal = {{"H", "H"}, {"G", "H"}, {"I", "F"}, {"I", "F"}, {"G", "H"}};
-            break;
-        case 3:
-            mReuse = {true, true, true, true, true};
-            mSucc = {"C", "D", "E", "F", "G"};
-            mGoal = {{"G", "E"}, {"F", "G"}, {"C", "C"}, {"D", "D"}, {"D", "C"}};
-            break;
-        case 4:
-            mReuse = {false, false, false, false};
-            mSucc = {"D", "E", "F", "G"};
-            mGoal = {{"G"}, {"F"}, {"G"}, {"F"}};
-            break;
-        default:
-            break;
-        }
-    }
-
-    void Geyser::getGeyser()
-    {
-        for (u16 wave = 0; wave < 3; ++wave)
-        {
-            // ??????
-            std::vector<bool> tReuse;
-            std::vector<std::string> tSucc;
-            std::vector<std::vector<std::string>> tGoal;
-            std::copy(mSucc.begin(), mSucc.end(), std::back_inserter(tSucc));
-            std::copy(mReuse.begin(), mReuse.end(), std::back_inserter(tReuse));
-            std::copy(mGoal.begin(), mGoal.end(), std::back_inserter(tGoal));
-
-            grnd.init(mGameSeed[wave]);
-            for (u16 i = 0; i < 15; ++i)
-            {
-                for (u64 sel = tSucc.size() - 1; sel > 0; --sel)
-                {
-                    u64 index = (grnd.getU32() * (sel + 1)) >> 0x20;
-                    std::swap(tSucc[sel], tSucc[index]);
-                    std::swap(tReuse[sel], tReuse[index]);
-                    std::swap(tGoal[sel], tGoal[index]);
-                }
-                mPosition[wave] += tSucc[0];
-                // mPosition[wave] += "(" + mSucc[0] + mGoal[0][!mReuse[0] ? 0 : (grnd.getU32() * (mGoal[0].size())) >> 0x20] + ")";
-                // mPosition[wave] += "(" + mSucc[0] + mGoal[0][!mReuse[0] ? 0 : (grnd.getU32() * (mGoal[0].size())) >> 0x20] + ")";
+                // printf("RN-> %08X %d\n", random, mRareId);
             }
         }
+        printf("\n");
+        // for (u16 time = 0; time < 35; ++time)
+        // {
+        //     mWaveRnd.init(rnd.getU32());
+        //     u16 mRareId = 0;
+        //     for (u64 mProb = 0; mProb < 7; ++mProb)
+        //     {
+        //         if (!(mWaveRnd.getU32() * (mProb + 1) >> 0x20))
+        //             mRareId = mProb;
+        //     }
+        // }
     }
+    // u32 Ocean::getWaveInfo()
+    // {
+    //     const u16 mEvent[7] = {18, 1, 1, 1, 1, 1, 1};
+    //     const u16 mTide[3] = {1, 3, 1};
+    //     rnd.init(mGameSeed);
+
+    //     for (u16 wave = 0; wave < 3; ++wave)
+    //     {
+    //         for (u64 event = 0, sum = 0; event < 7; ++event) // Calculate Wave Event
+    //         {
+    //             if ((wave > 0) && (mWave.event[wave - 1] != 0) && (mWave.event[wave - 1] == event))
+    //                 continue;
+    //             sum += mEvent[event];
+    //             if ((rnd.getU32() * sum >> 0x20) < mEvent[event])
+    //                 mWave.event[wave] = event;
+    //         }
+    //         for (u64 tide = 0, sum = 0; tide < 3; ++tide) // Calculate Wave Tide
+    //         {
+    //             if ((tide == 0) && (1 <= mWave.event[wave] && (mWave.event[wave] <= 3)))
+    //                 continue;
+    //             sum += mTide[tide];
+    //             if ((rnd.getU32() * sum >> 0x20) < mTide[tide])
+    //                 mWave.event[wave] == 6 ? mWave.tide[wave] = 0 : mWave.tide[wave] = tide;
+    //         }
+    //     }
+    //     u32 result = mWave.tide[0] * 100000 + mWave.event[0] * 10000 + mWave.tide[1] * 1000 + mWave.event[1] * 100 + mWave.tide[2] * 10 + mWave.event[2];
+    //     return result;
+    // }
+
+    // RareArray Ocean::getRareArray()
+    // {
+    //     sead::Random mWaveRnd;
+    //     RareArray mEnemy;
+    //     // uint16_t mEnemyArray[66];
+
+    //     for (u16 wave = 0, mWave = 0; wave < 3; ++wave)
+    //     {
+    //         sead::Random rnd;
+    //         rnd.init(mWaveSeed[wave]);
+    //         rnd.getU32();
+    //         for (u16 time = 0; time < 35; ++time)
+    //         {
+    //             mWaveRnd.init(rnd.getU32());
+    //             u16 mRareId = 0;
+    //             for (u64 mProb = 0; mProb < 7; ++mProb)
+    //             {
+    //                 if (!(mWaveRnd.getU32() * (mProb + 1) >> 0x20))
+    //                     mRareId = mProb;
+    //             }
+    //             if (mEnemy.mTime[wave][time])
+    //             {
+    //                 mEnemy.mArray[mWave] = mRareId;
+    //                 mWave++;
+    //             }
+    //         }
+    //     }
+    //     return mEnemy;
+    // }
+
+    // bool Ocean::getGeyserValue(u16 stage, u16 wave)
+    // {
+    //     rnd.init(mGameSeed);
+    //     rnd.getU32();
+    //     mGeyserSeed[0] = mGameSeed;
+    //     mGeyserSeed[1] = rnd.getU32();
+    //     mGeyserSeed[2] = rnd.getU32();
+
+    //     switch (stage)
+    //     {
+    //     case 0:
+    //         mReuse = {true, true, true, true, true, true, true, true, true};
+    //         mSucc = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+    //         mDist = {999, 999, 999, 999, 167, 300, 188, 465, 316};
+    //         break;
+    //     case 1:
+    //         mReuse = {true, true, true, true, true, true, true, true};
+    //         mSucc = {"A", "B", "C", "D", "E", "F", "G", "H"};
+    //         mDist = {999, 999, 999, 999, 80, 157, 235, 424};
+    //         break;
+    //     case 2:
+    //         mReuse = {true, true, true, true, true};
+    //         mSucc = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+    //         mDist = {999, 999, 999, 999, 258, 329, 199, 366, 225};
+    //         break;
+    //     case 3:
+    //         mReuse = {true, true, true, true, true, true, true};
+    //         mSucc = {"A", "B", "C", "D", "E", "F", "G"};
+    //         mDist = {999, 999, 405, 348, 165, 131, 151};
+    //         break;
+    //     case 4:
+    //         mReuse = {true, true, true, true, true, true, true};
+    //         mSucc = {"A", "B", "C", "D", "E", "F", "G"};
+    //         mDist = {999, 999, 999, 999, 0, 999, 999, 999};
+    //         break;
+    //     default:
+    //         break;
+    //     }
+
+    //     // switch (stage)
+    //     // {
+    //     // case 0:
+    //     //     mReuse = {true, true, true, true, true};
+    //     //     mSucc = {"E", "F", "G", "H", "I"};
+    //     //     mDist = {167, 300, 188, 465, 316};
+    //     //     break;
+    //     // case 1:
+    //     //     mReuse = {true, false, true, true};
+    //     //     mSucc = {"E", "F", "G", "H"};
+    //     //     mDist = {80, 157, 235, 424};
+    //     //     break;
+    //     // case 2:
+    //     //     mReuse = {false, true, true, true, true};
+    //     //     mSucc = {"A", "B", "G", "H", "I"};
+    //     //     mDist = {258, 329, 199, 366, 225};
+    //     //     break;
+    //     // case 3:
+    //     //     mReuse = {true, true, true, true, true};
+    //     //     mSucc = {"C", "D", "E", "F", "G"};
+    //     //     mDist = {405, 348, 165, 131, 151};
+    //     //     break;
+    //     // case 4:
+    //     //     mReuse = {false, false, false, false};
+    //     //     mSucc = {"D", "E", "F", "G"};
+    //     //     mDist = {999, 0, 999, 999};
+    //     //     break;
+    //     // default:
+    //     //     break;
+    //     // }
+
+    //     u16 minValue = *std::min_element(mDist.begin(), mDist.end());
+    //     sead::Random grnd;
+    //     grnd.init(mGeyserSeed[wave]);
+
+    //     std::vector<bool> tReuse;
+    //     std::vector<std::string> tSucc;
+    //     std::vector<u16> tDist;
+
+    //     std::copy(mSucc.begin(), mSucc.end(), std::back_inserter(tSucc));
+    //     std::copy(mReuse.begin(), mReuse.end(), std::back_inserter(tReuse));
+    //     std::copy(mDist.begin(), mDist.end(), std::back_inserter(tDist));
+
+    //     for (u16 idx = 0; idx < N; ++idx)
+    //     {
+    //         for (u64 sel = mSucc.size() - 1; sel > 0; --sel)
+    //         {
+    //             u64 index = (grnd.getU32() * (sel + 1)) >> 0x20;
+    //             std::swap(tSucc[sel], tSucc[index]);
+    //             std::swap(tReuse[sel], tReuse[index]);
+    //             std::swap(tDist[sel], tDist[index]);
+    //         }
+    //         if (tReuse[0])
+    //             grnd.getU32();
+
+    //         if (tDist[0] != minValue)
+    //             return false;
+    //     }
+    //     return true;
+    // }
 }; // namespace Coop

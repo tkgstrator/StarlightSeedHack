724ff02fdcb6f3b20583dcbb5c0eb2045ef53b91
tkgstrator <nasawake.am@gmail.com>
2020-11-05 21:27:49 -0500

    Fix Memory issue

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 from library import Ocean
 
 FILENAME = "202020.txt"
+OUTPUT = "Output.txt"
+
+# FILENAME = "Output.txt"
+# OUTPUT = "Output[1].txt"
+
+
+def isValidWave(mWaveMgr):
+
+    for wave in range(3):  # WAVE3までをループ
+        mSum = [0, 0, 0, 0, 0, 0, 0]  # オオモノの湧き数を保存
+        for index, line in enumerate(mWaveMgr[wave].mBossAppearIds):
+            mAppearId = line[0]  # 0番目の要素は湧き方向
+            mBoss = line[1]  # 1番目の要素は湧いたオオモノ配列
+    return True  # NG条件を見たなさなければTrueを返す
+
+
 if __name__ == "__main__":
-    with open(FILENAME, mode="r") as f:
-        for mSeed in f:
-            mOcean = Ocean.Ocean(int(mSeed))
-            mOcean.setWaveMgr() # 常に満潮昼であることはわかっているので無視してよい
-            print(mOcean.mEvent, mOcean.mTide)
-            print(mOcean.mWaveMgr[0].mBossAppearIds)
-            print(mOcean.mWaveMgr[1].mBossAppearIds)
-            print(mOcean.mWaveMgr[2].mBossAppearIds)
-            break
-        # print(mOcean.mWaveMgr[0].mAppearIds, mOcean.mWaveMgr[0].mBossIds)
-        # print(mOcean.mWaveMgr[1].mAppearIds, mOcean.mWaveMgr[1].mBossIds)
-        # print(mOcean.mWaveMgr[2].mAppearIds, mOcean.mWaveMgr[2].mBossIds)
+    with open(OUTPUT, mode="w") as fw:
+        mProgress = 0x0
+        mOcean = Ocean.Ocean()
+        with open(FILENAME, mode="r") as f:
+            for mLine in f:
+                mSeed = int("0x" + mLine, 16)
+                mOcean.init(mSeed)
+                mOcean.setWaveMgr()  # 常に満潮昼であることはわかっているので無視してよい
+                if mSeed >= mProgress * 0x100000:
+                    # print("\rmSeed->", hex(m:w
+                    # Seed), end="")
+                    mProgress += 1
+                if isValidWave(mOcean.mWaveMgr):
+                    fw.write(mLine)
+
+                counter = [0, 0, 0, 0, 0, 0, 0]
+                for idx in range(7):
+                    counter[idx] += mOcean.mWaveMgr[2].mBossIds[idx]
+                print(mLine.strip(), counter)
+
+                # 途中で処理を終わらせたいときの条件
+                if mSeed >= 0x100:
+                    break

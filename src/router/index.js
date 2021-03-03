e569105f90c43c5f87451fac05b907cedb1de08c
tkgstrator <tkgstrator@users.noreply.github.com>
2021-03-03 22:44:29 +0900

    Fix Redirects

--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,56 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/simulator',
-    name: 'Simulator',
+    path: "/simulator",
+    name: "Simulator",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Random.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Random.vue"),
   },
   {
-    path: '/oceancalc',
-    name: 'OceanCalc',
+    path: "/oceancalc",
+    name: "OceanCalc",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/OceanCalc.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/OceanCalc.vue"),
   },
   {
-    path: '/dev',
-    name: 'Dev',
+    path: "/dev",
+    name: "Dev",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Dev.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ "../views/Dev.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: "hash",
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
